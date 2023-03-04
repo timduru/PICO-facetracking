1c7e46d6d6d4dc207f9220981e74dfca048a11e6
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-03-04 18:12:05 +0100

    Feature: service 1/2

--- Assets/FacetrackingSender.java
+++ Assets/FacetrackingSender.java
@@ -9,57 +9,69 @@ import android.os.Build;
 import android.os.IBinder;
 
 public class FacetrackingSender extends Service {
+
     private static final String CHANNEL_ID = "ForegroundServiceChannel";
-    private boolean isRunning = false;
     private Thread senderThread = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        try {
-            final UDPSocket send = new UDPSocket();
-            send.Client("192.168.1.101", 27000);
-            isRunning = true;
-            senderThread = new Thread(() -> {
-                while (/*isRunning*/true) { // TODO as it's not an atomic bool this won't work
-                    try {
-                        send.Send("hello?");
-                        Thread.sleep(5000);
-                    } catch (Exception e) {}
-                }
-            });
-        } catch (Exception ex) {
-            System.err.println(ex.toString());
-        }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (this.senderThread == null) return START_STICKY; // onCreate failed to setup senderThread
+        String input = intent.getStringExtra("inputExtra");
+
+        createNotificationChannel();
 
         Notification notification = new Notification.Builder(this, CHANNEL_ID)
-                .setContentTitle("FacetrackingSender")
-                .setContentText("Sending facetracking data")
+                .setContentTitle("Foreground Service")
+                .setContentText(input)
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .build();
+
         startForeground(1, notification);
 
-        if (!senderThread.isAlive()) senderThread.start();
+        this.senderThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UDPSocket send = new UDPSocket();
+                    send.Client("192.168.1.101", 27000);
 
-        return START_STICKY;
+                    while (true) {
+                        send.Send("hello?");
+                        Thread.sleep(2000);
+                    }
+                } catch (Exception ex) {
+                    System.err.printf(ex.toString()); // TODO ignore interrrupted sleep
+                }
+            }
+        });
+        this.senderThread.start();
+
+        return START_NOT_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (senderThread == null) return;
-        
-        isRunning = false;
-        try {
-            senderThread.join();
-        } catch (Exception ex) {
-            System.err.println(ex.toString());
+
+        if (this.senderThread != null) {
+            this.senderThread.interrupt();
+            this.senderThread = null; // it's closed now
         }
     }
 
